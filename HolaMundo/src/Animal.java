0a7d70387980611b1ccac5ffa1657f7ba4a85229
Lino Vidal <saracyan78@hotmail.com>
2019-10-01 17:58:26 +0200

    Segunda proba de codigo con retranca.

---
+++ HolaMundo/src/Animal.java
@@ -0,0 +1,8 @@
+
+public class Animal 
+{
+	public Animal() 
+	{
+		System.out.println("Aqui van a situarse os diferentes animais e as suas onomatopeyas");  
+	}
+}
--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-
+		new Animal();
 	}
 
 }
